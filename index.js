79b29b33db516a982c9445af52484b213729af67
fulun <fulun2010@163.com>
2018-06-13 16:07:12 +0800

    init project

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-import swiper from './swiper/swiper.vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+require('swiper/dist/css/swiper.css')
+import SwiperComponent from './swiper/swiper.vue'
 
-export default swiper
+const swiper = SwiperComponent
+const install = function (Vue, globalOptions) {
+  if (globalOptions) {
+    SwiperComponent.props.globalOptions.default = () => globalOptions
+  }
+  Vue.component(SwiperComponent.name, SwiperComponent)
+}
+const Smiley = {  install }
+
+export default Smiley
